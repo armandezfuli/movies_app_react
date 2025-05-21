ad83bdcc1d0e8458dfc1d52aa7ba91f3d2e1a799
Arman Dezfuli <armandezfuli@proton.me>
2025-05-21 11:26:22 +0330

    feat(header): add Header and Search components + complete Tailwind theme, layers and utilities

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import React, {useState} from 'react'
+import Search from "./components/Search.jsx";
 
+const App = () => {
 
-function App() {
+    const [searchTerm, setSearchTerm] = useState("")
 
 
     return (
-        <h1 className="text-3xl text-blue-600">test</h1>
+        <main>
+            <div className="pattern" />
+
+            <div className="wrapper">
+                <header>
+                    <img src="../public/hero.png" alt="hero" />
+                    <h1>Find <span className="text-gradient">Movies</span> You’ll Love Without the Hassle</h1>
+                </header>
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            </div>
+
+        </main>
     )
 }
-
 export default App
