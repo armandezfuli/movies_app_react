dbe3d937c14a1992e8a829ca1572dff134be8cc7
Arman Dezfuli <armandezfuli@proton.me>
2025-05-26 17:53:12 +0330

    feat(search): integrate Appwrite to track trending movie searches

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,14 @@ import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
 import MovieCard from "./components/MovieCard.jsx";
 import {useDebounce} from "react-use";
+import {updateSearchCount} from "./appwrite.js";
 
 const API_BASE_URL = "https://api.themoviedb.org/3/"
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY
 
 const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        authorization: `Bearer ${API_KEY}`,
+    method: 'GET', headers: {
+        accept: 'application/json', authorization: `Bearer ${API_KEY}`,
     }
 }
 
@@ -20,7 +19,7 @@ const App = () => {
     const [errorMessage, setErrorMessage] = useState("")
     const [movies, setMovies] = useState([])
     const [loading, setLoading] = useState(false)
-    const [debouncedSearchTerm,setDebouncedSearchTerm] = useState("")
+    const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("")
 
     useDebounce(() => setDebouncedSearchTerm(searchTerm), 1000, [searchTerm])
 
@@ -28,18 +27,23 @@ const App = () => {
         setLoading(true)
         setErrorMessage('')
         try {
-            const endpoint = query ? `${API_BASE_URL}/search/movie?query=${encodeURI(query)}` :`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+            const endpoint = query ? `${API_BASE_URL}/search/movie?query=${encodeURI(query)}` : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
             const response = await fetch(endpoint, API_OPTIONS)
             if (!response.ok) {
                 throw Error('Failed to fetch movies')
             }
             const data = await response.json()
+
             if (data.Response === 'false') {
                 setErrorMessage(data.Error || 'Failed to fetch movies')
                 setMovies([])
                 return
             }
             setMovies(data.results || [])
+
+            if (query && query.length > 0) {
+                await updateSearchCount(query, data.results[0])
+            }
         } catch (e) {
             console.log(e)
             setErrorMessage("Error fetching movies")
@@ -50,34 +54,23 @@ const App = () => {
 
     useEffect(() => {
         fetchMovies(debouncedSearchTerm)
-    },[debouncedSearchTerm])
+    }, [debouncedSearchTerm])
 
-    return (
-        <main>
-            <div className="pattern" />
-            <div className="wrapper">
-                <header>
-                    <img src="../public/hero.png" alt="hero" />
-                    <h1>Find <span className="text-gradient">Movies</span> You’ll Love Without the Hassle</h1>
-                    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-                </header>
-                <section className="all-movies">
-                    <h2 className='mt-10'>All movies</h2>
-                    {loading ? (
-                        <Spinner />
-                    ) : errorMessage ? (
-                        <p className="text-red-500">{errorMessage}</p>
-                    ) : (
-                     <ul>
-                         {movies.map(movie => (
-                             <MovieCard key={movie.id} movie={movie}/>
-                         ))}
-                     </ul>
-                    )}
-                </section>
-            </div>
-
-        </main>
-    )
+    return (<main>
+        <div className="pattern"/>
+        <div className="wrapper">
+            <header>
+                <img src="../public/hero.png" alt="hero"/>
+                <h1>Find <span className="text-gradient">Movies</span> You’ll Love Without the Hassle</h1>
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+            </header>
+            <section className="all-movies">
+                <h2 className='mt-10'>All movies</h2>
+                {loading ? (<Spinner/>) : errorMessage ? (<p className="text-red-500">{errorMessage}</p>) : (<ul>
+                    {movies.map(movie => (<MovieCard key={movie.id} movie={movie}/>))}
+                </ul>)}
+            </section>
+        </div>
+    </main>)
 }
 export default App
