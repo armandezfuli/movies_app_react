8cedc8abb5a4c6d1f2ae6508e2aeef1a2457a896
Arman Dezfuli <armandezfuli@proton.me>
2025-05-26 22:53:32 +0330

    fix(): hero and search icon path

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ const App = () => {
         <div className="pattern"/>
         <div className="wrapper">
             <header>
-                <img src="../public/hero.png" alt="hero"/>
+                <img src="/hero.png" alt="hero"/>
                 <h1>Find <span className="text-gradient">Movies</span> You’ll Love Without the Hassle</h1>
                 <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
             </header>
